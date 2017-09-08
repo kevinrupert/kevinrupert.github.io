ddcd3df3d91aa94937a48af9d5b5d62719915488
kevinrupert <kevinrupert.design@gmail.com>
2017-09-08 18:40:38 -0400

    js

--- animation/assets/js/main.js
+++ animation/assets/js/main.js
@@ -1,21 +1,32 @@
 // init Masonry
 
-var $grid = $('.grid').masonry({
+var $grid = $('#grid').masonry({
   itemSelector: '.grid-item',
   columnWidth: '.grid-sizer',
   percentPosition: true
 });
+
 // layout Masonry after each image loads
 $grid.imagesLoaded().progress( function() {
   $grid.masonry('layout');
 });
 
-var posterCount = 12;
+var posterCount = $('video').length;
 var postersLoaded = 0;
 
 $('video').load(function () {
-  postersLoaded++;
-  if (postersLoaded = posterCount) {
-    $grid.masonry('layout');
-  }
+
+    postersLoaded++;
+
+    if (postersLoaded >= posterCount) {
+
+
+        $('#grid').masonry({
+            itemSelector: '.grid-item'
+        });
+
+        $('#grid').masonry('reloadItems');
+        $('#grid').masonry('layout');
+
+    }
 });
