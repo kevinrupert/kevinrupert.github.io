5749f505b0c960420d91f7e2346e8d3553304b86
kevinrupert <kevinrupert.design@gmail.com>
2021-03-16 10:42:54 -0400

    New home page layout.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,36 @@
-$('.menu-icon').click(function() {
-    if (document.getElementById("menu").classList.contains('open')) {
-      $('.menu, .icon-container').removeClass('open');
-      const body = document.body;
-      const scrollY = body.style.top;
-      body.style.position = '';
-      body.style.top = '';
-      window.scrollTo(0, parseInt(scrollY || '0') * -1);
-      $('.model').removeClass('overflow');
-    }
-    else {
-      $('.menu, .icon-container').addClass('open');
-      const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
-      const body = document.body;
-      body.style.position = 'fixed';
-      body.style.top = `-${scrollY}`;
-      $('.model').addClass('overflow');
-    }
-    $('.menu-icon').removeClass('highlight');
-  }
-);
+// $('.menu-icon').click(function() {
+//     if (document.getElementById("menu").classList.contains('open')) {
+//       $('.menu, .icon-container').removeClass('open');
+//       const body = document.body;
+//       const scrollY = body.style.top;
+//       body.style.position = '';
+//       body.style.top = '';
+//       window.scrollTo(0, parseInt(scrollY || '0') * -1);
+//       $('.model').removeClass('overflow');
+//     }
+//     else {
+//       $('.menu, .icon-container').addClass('open');
+//       const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+//       const body = document.body;
+//       body.style.position = 'fixed';
+//       body.style.top = `-${scrollY}`;
+//       $('.model').addClass('overflow');
+//     }
+//     $('.menu-icon').removeClass('highlight');
+//   }
+// );
 
-window.addEventListener('scroll', () => {
-  document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
-});
+// window.addEventListener('scroll', () => {
+//   document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+// });
 
-$('.animation-group').waypoint(function() {
-  $(this.element).find('.animate-fade, .animate-fade-up, .animate-scale, .animate-width').addClass('animated');
-}, {offset: '90%' });
+// $('.animation-group').waypoint(function() {
+//   $(this.element).find('.animate-fade, .animate-fade-up, .animate-scale, .animate-width').addClass('animated');
+// }, {offset: '90%' });
 
-$('.animate-fade, .animate-fade-up, .animate-scale, .animate-width').waypoint(function() {
-  $(this.element).addClass('animated');
-}, {offset: '90%' });
+// $('.animate-fade, .animate-fade-up, .animate-scale, .animate-width').waypoint(function() {
+//   $(this.element).addClass('animated');
+// }, {offset: '90%' });
 
 // var myElement = $('.13-17');
 
